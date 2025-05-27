39737d9404d2f032395c343cf8a61a3bbdfcca5d
Garrett Maitland <gmaitlan18@gmail.com>
2025-05-27 19:29:09 -0400

    feat: create 16x16 grid for Etch-a-Sketch

---
+++ js/etch-a-sketch.js
@@ -0,0 +1,16 @@
+createGrid(16, 16);
+
+function createGrid(numberOfRows, numberOfColumns) {
+  let etchASketchView = document.querySelector(".etch-a-sketch-display");
+  for (let i = 0; i < numberOfRows; i++) {
+    let etchASketchRow = document.createElement("div");
+    etchASketchRow.classList.add("etch-a-sketch-row");
+    for (let j = 0; j < numberOfColumns; j++) {
+      console.log(`${i}, ${j}`);
+      let etchASketchTile = document.createElement("div");
+      etchASketchTile.classList.add("etch-a-sketch-tile");
+      etchASketchRow.appendChild(etchASketchTile);
+    }
+    etchASketchView.appendChild(etchASketchRow);
+  }
+}
