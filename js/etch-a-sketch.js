da6827957c984c42856f852ec1fe3ad6b0fe3329
Garrett Maitland <gmaitlan18@gmail.com>
2025-05-27 20:26:38 -0400

    feat: add ability to resize the etch-a-sketch

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,5 +1,12 @@
 createGrid(16, 16);
 
+let resizeBtn = document.querySelector(".resize-button");
+if (resizeBtn !== null) {
+  resizeBtn.addEventListener("click", resizeEtchASketch);
+} else {
+  alert("Resize button null!");
+}
+
 function createGrid(numberOfRows, numberOfColumns) {
   let etchASketchView = document.querySelector(".etch-a-sketch-display");
   for (let i = 0; i < numberOfRows; i++) {
@@ -14,4 +21,33 @@ function createGrid(numberOfRows, numberOfColumns) {
     }
     etchASketchView.appendChild(etchASketchRow);
   }
+}
+
+function clearGrid() {
+  let etchASketchRows = document.querySelectorAll(".etch-a-sketch-row");
+  etchASketchRows.forEach((etchASketchRow) => {
+    etchASketchRow.remove();
+  });
+}
+
+function resizeEtchASketch() {
+  let numOfRowsAndCols = promptForNumOfRowsAndCols();
+  clearGrid();
+  createGrid(numOfRowsAndCols, numOfRowsAndCols);
+}
+
+function isValidGridNumber(number) {
+  if (Number.isNaN(number) || (number < 1) || (number > 100)) {
+    return false;
+  }
+  return true;
+}
+
+function promptForNumOfRowsAndCols() {
+  let numOfRowsAndCols;
+  do {
+    numOfRowsAndCols = parseInt(prompt("Enter the number of rows and columns"));
+  } while (!isValidGridNumber(numOfRowsAndCols));
+
+return numOfRowsAndCols;
 }
